df309666a3f243748c622adcb40ffb0c59450c75
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-22 15:26:52 -0500

    imported blogs table into index file

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,15 @@
 import * as express from 'express';
+import config from "../config";
+import Blog from "./blog";
 
 const router = express.Router();
 
 router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
+
+router.get("/api/blogs", async (req, res) => {
+
+})
 
 export default router;
